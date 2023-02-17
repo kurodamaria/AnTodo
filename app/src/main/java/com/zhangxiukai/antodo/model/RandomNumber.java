97c84204ab6e919ae1d088b3494102a7018c4d1c
kurodamaria <xfreunion@gmail.com>
2023-02-17 13:13:14 +0800

    first commit

---
+++ app/src/main/java/com/zhangxiukai/antodo/model/RandomNumber.java
@@ -0,0 +1,9 @@
+package com.zhangxiukai.antodo.model;
+
+public class RandomNumber {
+    private int randomNumber;
+
+    public String getText() {
+        return String.valueOf(randomNumber);
+    }
+}
